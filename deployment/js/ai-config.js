fa166a10f12d40dbe04335ade304b88687464af4
Alexandre <alexandrefornari@gmail.com>
2012-03-22 13:56:52 -0300

    Modificação no tamanho e retirada da função uninit() do javascript.

--- deployment/js/ai-config.js
+++ deployment/js/ai-config.js
@@ -1,15 +1,15 @@
 // Variáveis do AI_Loader
 var flashvars = {};
 flashvars.ai = "swf/AI-0085.swf";
-flashvars.width = "700";
-flashvars.height = "400";
+flashvars.width = "600";
+flashvars.height = "343";
 
 // Parâmetros do player (Flash)
 var params = {};
 params.menu = "false";
-params.scale = "noscale";
+params.scale = "showall";
 
 // Atributos da tag HTML que contém o SWF
 var attributes = {};
 attributes.id = "ai";
-attributes.align = "middle";
+attributes.align = "left";
